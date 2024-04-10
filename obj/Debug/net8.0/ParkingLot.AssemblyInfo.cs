500d2724a1c0ad655c757b31a18ea3679aa63b09
vala0905 <vala220805@gmail.com>
2024-04-10 14:10:33 -0600

    Web API template created Models folder and files added

--- obj/Debug/net8.0/ParkingLot.AssemblyInfo.cs
+++ obj/Debug/net8.0/ParkingLot.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ParkingLot")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+b38e8db9743c2d16f06c8ff99f57a6a12c57a0fb")]
 [assembly: System.Reflection.AssemblyProductAttribute("ParkingLot")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ParkingLot")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
